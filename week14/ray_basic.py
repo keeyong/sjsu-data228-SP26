a656cee0389e87f57bc77dbda59e70282fb80b2c
Keeyong <keeyonghan@hotmail.com>
2026-04-19 13:43:26 -0700

    Set up Ray for parallel function execution
    Initialize Ray and define a remote function for parallel execution.

--- week14/ray_basic.py
+++ week14/ray_basic.py
@@ -1,16 +1,13 @@
 import ray
 import time
 
-# 1. Ray 시작 (로컬 클러스터 생성)
 ray.init(include_dashboard=True)
 
-# 2. 병렬 실행할 함수
 @ray.remote
 def work(x):
     time.sleep(2)
     return x * x
 
-# 3. 병렬 실행
 start = time.time()
 
 futures = [work.remote(i) for i in range(5)]
